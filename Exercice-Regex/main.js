2e2628f842be3fa44b377e3549354937a2b6beba
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-01-22 12:33:39 +0100

    Update Exo regex

--- Exercice-Regex/main.js
+++ Exercice-Regex/main.js
@@ -5,19 +5,24 @@
 //Si la condition est fausse je renvoie vrai.
 
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const number = ["0", "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"];
+const mail = ["-" , "." , "@" , "_"];
+const newTable = [...alphabet , ...number , ...mail]
+// console.log(newTable);
 //On initie une variable permettant de récupérer une chaine de caractère entrée par l'utilisateur
 let username = document.getElementById("name").value;
+let userPhoneNumber = document.getElementById("phone").value;
+let emailUser = document.getElementById("email").value;
+// console.log(userPhoneNumber);
 // let username = "N1co";
 // console.log(username);
 
 //On construit une fonction permettant de controler si la donnée rentré par l'utilisateur est bonne ou non par rapport au tableau
 //On doit mettre un paramètre générique pour réutiliser la fonction dans le programme
-function testAlpha (chaine) {
-    //On fait une boucle for pour parcourir la chaine de caractère entrée par l'utilisateur
-    for (let i = 0; i < chaine.length; i++) {
+function testAlpha (input , arrayCompare) {
+    for (let i = 0; i < input.length; i++) {
         //On test si la valeur saisie par l'utilisateur est vraie ou fausse
-        console.log(alphabet.includes(chaine[i]));
-        if (alphabet.includes(chaine[i]) === false) {
+        if (arrayCompare.includes(input[i]) === false){
             //Si la condition est remplie on renvoie false
             return false
         }
@@ -26,4 +31,6 @@ function testAlpha (chaine) {
     return true
 };
 
-console.log(testAlpha(username));
+console.log(testAlpha(username , newTable));
+console.log(testAlpha(userPhoneNumber , newTable));
+console.log(testAlpha(emailUser , newTable));
